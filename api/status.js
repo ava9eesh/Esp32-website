d7c973b54f7068141d9c45d92575cd7178e97d13
ava9eesh <avaneeshshinde23@gmail.com>
2026-01-02 23:43:49 +0530

    Update status.js

--- api/status.js
+++ api/status.js
@@ -1,1 +1,13 @@
+let lastData = {
+  time: "--:--:--",
+  emergency: false,
+  location: "Unknown"
+};
 
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    lastData = req.body;
+    return res.status(200).json({ ok: true });
+  }
+  res.status(200).json(lastData);
+}
